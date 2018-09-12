81cb5c234fadee42831bbf056bb8feae603376e0
Alex Puhl <apuhlswimmer@hotmail.com>
2018-09-12 11:56:39 -0600

    changed server port to 8000

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 8000,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
   JWT_SECRET: process.env.JWT_SECRET,
